91011b6dcbc081359eef8cb2742b5faca98d107c
lakshya-roonwal <erenpool7214@gmail.com>
2024-07-20 12:23:49 +0530

    revert resume.tsx

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -1,120 +1,91 @@
 import { ResumeData } from "@/types/Resume.type";
 import React, { FC } from "react";
 
-const styles = {
-  nameFontSize: "31.2px",
-  sectionTitleFontSize: "18.2px",
-  itemTitleFontSize: "15.6px",
-  itemSubTitleFontSize: "14.3px",
-  normalTextFontSize: "14.3px",
-  sectionMarginBottom: "5.2px",
-  borderColor: "#4A4A4A",
-  sectionTitleBorderWidth: "1px",
-  itemSpacing: "8px",
-};
 
-interface ResumeProps {
-  data: ResumeData;
+const styles={
+  nameFontSize:"31.2px",
+  sectionTitleFontSize:"18.2px",
+  itemTitleFontSize:"15.6px",
+  itemSubTitleFontSize:"14.3px",
+  normalTextFontSize:"14.3px",
+  sectionMarginBottom:"5.2px",
 }
 
-const Resume: FC<ResumeProps> = ({ data }) => {
+interface ResumeProps {
+  data:ResumeData  
+}
+
+
+const Resume: FC<ResumeProps> = ({data}) => {
   return (
     <div
-      style={{
-        width: "21cm",
-        height: "29.7cm",
-        padding: "26px",
-        backgroundColor: "#f9f9f9",
-        color: "#333",
-        boxShadow: "0 0 15px rgba(0, 0, 0, 0.1)",
-        fontFamily: "Arial, sans-serif",
-        lineHeight: "1.5",
-      }}
+      style={{ width: "21cm", height: "29.7cm"}}
+      className={"p-[26px] bg-white text-black outline  ibm-plex-serif"}
     >
-      <header className="flex flex-col items-center mb-4">
-        <h1 style={{ fontSize: styles.nameFontSize }} className="font-bold tracking-tighter text-blue-600">
-          {data.basics.name}
-        </h1>
-        <div className="flex flex-wrap justify-center text-sm mt-2">
-          <div className="flex items-center mx-2">
-            <span style={{ fontSize: styles.normalTextFontSize }}>{data.basics.location}</span>
+      <header className="flex flex-col items-center mb-[13px]">
+        <h1 style={{fontSize:styles.nameFontSize}} className="font-bold tracking-tighter">{data.basics.name}</h1>
+        <div className="flex justify-center text-[15.6px]">
+          <div className="flex items-center ml-[5.2px]">
+            <span style={{fontSize:styles.normalTextFontSize}} className="leading-tight">{data.basics.location}</span>
           </div>
-          <div className="flex items-center mx-2">
-            <span style={{ fontSize: styles.normalTextFontSize }}>{data.basics.email}</span>
+          <div className="flex items-center ml-[5.2px]">
+            <span style={{fontSize:styles.normalTextFontSize}} className="leading-tight">{data.basics.email}</span>
           </div>
-          <div className="flex items-center mx-2">
-            <span style={{ fontSize: styles.normalTextFontSize }}>{data.basics.phone}</span>
+          <div className="flex items-center ml-[5.2px]">
+            <span style={{fontSize:styles.normalTextFontSize}} className="leading-tight">{data.basics.phone}</span>
           </div>
-          <div className="flex items-center mx-2">
-            <a style={{ fontSize: styles.normalTextFontSize }} href={data.basics.linkedin} className="underline">
+          <div className="flex items-center ml-[5.2px]">
+            <a style={{fontSize:styles.normalTextFontSize}}  href={data.basics.linkedin} className="leading-tight underline">
               LinkedIn
             </a>
           </div>
-          <div className="flex items-center mx-2">
-            <a style={{ fontSize: styles.normalTextFontSize }} href={data.basics.github} className="underline">
+          <div className="flex items-center ml-[5.2px]">
+            <a style={{fontSize:styles.normalTextFontSize}}  href={data.basics.github} className="leading-tight underline">
               GitHub
             </a>
           </div>
         </div>
       </header>
 
-      <section style={{ marginBottom: styles.sectionMarginBottom }}>
-        <h2
-          style={{ fontSize: styles.sectionTitleFontSize, borderBottom: `${styles.sectionTitleBorderWidth} solid ${styles.borderColor}` }}
-          className="pb-1 text-gray-700"
-        >
+      <section style={{marginBottom:styles.sectionMarginBottom}}>
+        <h2 style={{fontSize:styles.sectionTitleFontSize}} className="text-lg font-medium border-b-[2px] border-black pb-[5.2px]">
           Education
         </h2>
         {data.education.map((edu, index) => (
-          <div key={index} style={{ marginBottom: styles.itemSpacing }}>
+          <div key={index} className="mb-2">
             <div className="flex justify-between items-end">
-              <span style={{ fontSize: styles.itemTitleFontSize }} className="font-bold">
-                {edu.college}
-              </span>
-              <span style={{ fontSize: styles.normalTextFontSize }} className="text-right">
+              <span style={{fontSize:styles.itemTitleFontSize}} className="leading-tight font-bold">{edu.college}</span>
+              <span style={{fontSize:styles.normalTextFontSize}} className="text-right">
                 {edu.startDate} - {edu.endDate}
               </span>
             </div>
             <div className="flex justify-between items-end">
-              <span style={{ fontSize: styles.itemSubTitleFontSize }} className="italic text-gray-600">
-                {edu.degree}
-              </span>
-              <span style={{ fontSize: styles.itemSubTitleFontSize }} className="italic text-right text-gray-600">
-                {edu.location}
-              </span>
+              <span style={{fontSize:styles.itemSubTitleFontSize}} className="font-extralight leading-tight  italic">{edu.degree}</span>
+              <span style={{fontSize:styles.itemSubTitleFontSize}} className="font-extralight leading-tight text-right italic">{edu.location}</span>
             </div>
           </div>
         ))}
       </section>
 
-      <section style={{ marginBottom: styles.sectionMarginBottom }}>
-        <h2
-          style={{ fontSize: styles.sectionTitleFontSize, borderBottom: `${styles.sectionTitleBorderWidth} solid ${styles.borderColor}` }}
-          className="pb-1 text-gray-700"
-        >
+      <section style={{marginBottom:styles.sectionMarginBottom}}>
+      <h2 style={{fontSize:styles.sectionTitleFontSize}} className="text-lg font-medium border-b-2 border-black pb-1">
           Experience
         </h2>
         {data.experience.map((exp, index) => (
-          <div key={index} style={{ marginBottom: styles.itemSpacing }}>
+          <div key={index} className="mb-2">
             <div className="flex justify-between items-end">
-              <span style={{ fontSize: styles.itemTitleFontSize }} className="font-bold">
-                {exp.role}
-              </span>
-              <span style={{ fontSize: styles.normalTextFontSize }} className="text-right">
+            <span style={{fontSize:styles.itemTitleFontSize}} className="leading-tight font-bold">{exp.role}</span>
+            <span style={{fontSize:styles.normalTextFontSize}} className="leading-tight text-right">
                 {exp.startDate} - {exp.endDate}
               </span>
             </div>
             <div className="flex justify-between items-end">
-              <span style={{ fontSize: styles.itemSubTitleFontSize }} className="italic text-gray-600">
-                {exp.company}
-              </span>
-              <span style={{ fontSize: styles.itemSubTitleFontSize }} className="italic text-right text-gray-600">
-                {exp.location}
-              </span>
+              <span style={{fontSize:styles.itemSubTitleFontSize}} className="leading-tight font-extralight  italic">{exp.company}</span>
+              <span style={{fontSize:styles.itemSubTitleFontSize}} className="leading-tight font-extralight  text-right italic">{exp.location}</span>
             </div>
-            <ul style={{ fontSize: styles.normalTextFontSize }} className="list-disc list-inside mt-2 pl-4">
+            <ul style={{fontSize:styles.normalTextFontSize}} className="list-disc list-inside mt-2 pl-4">
               {exp.details.map((detail, i) => (
-                <li key={i} style={{ fontSize: styles.normalTextFontSize }}>
+                <li key={i} className="leading-tight" style={{fontSize:styles.normalTextFontSize}}>
                   {detail}
                 </li>
               ))}
@@ -123,31 +94,26 @@ const Resume: FC<ResumeProps> = ({ data }) => {
         ))}
       </section>
 
-      <section style={{ marginBottom: styles.sectionMarginBottom }}>
-        <h2
-          style={{ fontSize: styles.sectionTitleFontSize, borderBottom: `${styles.sectionTitleBorderWidth} solid ${styles.borderColor}` }}
-          className="pb-1 text-gray-700"
-        >
+      <section style={{marginBottom:styles.sectionMarginBottom}}>
+      <h2 style={{fontSize:styles.sectionTitleFontSize}} className="text-lg font-medium border-b-2 border-black pb-1">
           Projects
         </h2>
         {data.projects.map((project, index) => (
-          <div key={index} style={{ marginBottom: styles.itemSpacing }}>
+          <div key={index} className="mb-2">
             <div className="flex justify-between items-end">
               <div className="flex">
-                <span style={{ fontSize: styles.itemTitleFontSize }} className="font-bold">
-                  {project.title} |
-                </span>
-                <span style={{ fontSize: styles.itemSubTitleFontSize }} className="italic text-gray-600 ml-1">
+              <span style={{fontSize:styles.itemTitleFontSize}} className="leading-tight font-bold">{project.title} |</span>
+                <span style={{fontSize:styles.itemSubTitleFontSize}}  className="leading-tight font-extralight italic ml-1">
                   {project.technologies.join(", ")}
                 </span>
               </div>
-              <span style={{ fontSize: styles.normalTextFontSize }} className="text-right">
+              <span style={{fontSize:styles.normalTextFontSize}} className="leading-tight text-right">
                 {project.startDate} - {project.endDate}
               </span>
             </div>
-            <ul style={{ fontSize: styles.normalTextFontSize }} className="list-disc list-inside mt-1 pl-4">
+            <ul style={{fontSize:styles.normalTextFontSize}} className="list-disc list-inside mt-[2.5px] pl-4">
               {project.details.map((detail, i) => (
-                <li key={i} style={{ fontSize: styles.normalTextFontSize }}>
+                <li key={i} className="text-sm">
                   {detail}
                 </li>
               ))}
@@ -156,21 +122,16 @@ const Resume: FC<ResumeProps> = ({ data }) => {
         ))}
       </section>
 
-      <section className="px-1">
-        <h2
-          style={{ fontSize: styles.sectionTitleFontSize, borderBottom: `${styles.sectionTitleBorderWidth} solid ${styles.borderColor}` }}
-          className="pb-1 text-gray-700"
-        >
+      <section className="px-[3.61px]">
+      <h2 style={{fontSize:styles.sectionTitleFontSize}} className="font-medium border-b-2 border-black pb-1">
           Technical Skills
         </h2>
         {Object.entries(data.skills).map(([category, skills], index) => (
-          <div key={index} className="mb-2">
-            <div className="flex items-center flex-wrap">
-              <span style={{ fontSize: styles.normalTextFontSize }} className="font-bold text-lg text-blue-600">
-                {category}:
-              </span>
-              {skills.map((skill: string, skillIndex: number) => (
-                <span key={skillIndex} style={{ fontSize: styles.normalTextFontSize }} className="ml-1">
+          <div key={index}>
+            <div className="flex items-center flex-wrap mb-[5.2px]">
+              <span style={{fontSize:styles.normalTextFontSize}} className="leading-tight text-lg font-bold">{category}:</span>
+              {skills.map((skill:string, skillIndex:number) => (
+                <span key={skillIndex} style={{fontSize:styles.normalTextFontSize}} className="ml-[6px] font-normal">
                   {skill}
                   {skillIndex < skills.length - 1 && ","}
                 </span>
